4e849a91a682de7ce228cdd6452cbe0c4d5d9342
CJN2D2 <juice@plurconsulting.com>
2026-02-16 01:24:02 +1100

    Delete letfishfuck_site_v2/letfishfuck_site_v2 4 directory

--- letfishfuck_site_v2/letfishfuck_site_v2 4/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-/* LFF shim: keep old references alive without split-brain */
-(function(){
-  if (window.__LFF_APPJS_LOADED__) return;
-  window.__LFF_APPJS_LOADED__ = true;
-  var s = document.createElement('script');
-  s.src = '/js/app.js?v=12';
-  s.defer = true;
-  document.head.appendChild(s);
-})();
